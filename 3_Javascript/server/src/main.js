4becf57d4243f3647131af86ead272444a9d5044
erlandM <erland.96@hotmail.com>
2026-02-11 11:24:23 +0100

    fixed the function to recognize 1 as not a prime

--- 3_Javascript/server/src/main.js
+++ 3_Javascript/server/src/main.js
@@ -31,6 +31,7 @@ for (let i=1; i<=99; i++){
 }
 
 function isPrime(num){
+    if (num <= 1) return num.toString();
     if (num%2===0 && num!==2) return num.toString();
     for (let i=3; i<=Math.sqrt(num); i+=2){
         if (num%i===0) return num.toString();

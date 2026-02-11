a8cbf236744b19f9849e4df9a104e37cadad8f08
erlandM <erland.96@hotmail.com>
2026-02-11 10:22:24 +0100

    Task 1 complete

---
+++ 3_Javascript/server/src/main.js
@@ -0,0 +1,8 @@
+let number=67;
+let string="Hello World";
+let bigInt=1234567890123456789012345678901234567890n;
+let boolean=true;
+let nullValue=null;
+let undefinedValue=undefined;
+let symbol=Symbol("unique");
+let object={name:"Ola", age:25};

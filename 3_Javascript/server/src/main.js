e87444e2803891c8dc2b60627990d022c54d76a9
stian <stiankarlsen709@gmail.com>
2026-02-12 15:08:58 +0100

    js oppgave 1

---
+++ 3_Javascript/server/src/main.js
@@ -0,0 +1,28 @@
+// Data typer
+
+// Number 
+let num = 1;
+
+// BigInt
+let bigI = 1234567890123456789012345n;
+
+// String 
+let str = "Hello World";
+
+// Boolean 
+let x = true;
+let y = false;
+
+// Undefined 
+let a;
+let j;
+
+// Null
+let k = null;
+
+// Symbol
+const d = Symbol();
+
+// Object 
+const person = {firstName: "jhon", lastName:"Doe"};
+

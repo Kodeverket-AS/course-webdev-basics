3341df2e9cdc3d17dd49bafad95385a6438d3669
erlandM <erland.96@hotmail.com>
2026-02-16 11:06:31 +0100

    Challene task 4 complete

--- CHALLENGE/main.js
+++ CHALLENGE/main.js
@@ -1,8 +1,8 @@
 async function getNewest () {
   try {
   const response = await fetch("https://hacker-news.firebaseio.com/v0/newstories.json")
-  const data = await response.json()
-  return data
+  const newestStories = await response.json()
+  return newestStories
 } 
 catch (error) {
   console.error("Error fetching newest stories:", error)
@@ -13,11 +13,35 @@ catch (error) {
 async function getStorybyId (id) {
   try {
     const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-    const data = await response.json()
-    return data
+    const story = await response.json()
+    return story
   } 
   catch (error) {
     console.error(`Error fetching story with ID ${id}:`, error)
     throw error
   }
+}
+
+function createArticleElement(getStorybyId) {
+  const article = document.createElement("article")
+  const title = document.createElement("h3")
+  const text = document.createElement("p")
+  const span = document.createElement("span")
+  const postedAt=document.createElement("p")
+  const link = document.createElement("a")
+
+  title.textContent = getStorybyId.title + " (" + getStorybyId.score + ")"
+  text.textContent = "text"
+  postedAt.textContent ="posted by " + getStorybyId.by + " at " + getStorybyId.time
+  link.textContent = "Read more"
+  link.href = getStorybyId.url
+  link.target = "_blank"
+
+  article.appendChild(title)
+  article.appendChild(text)
+  article.appendChild(span)
+  span.appendChild(postedAt)
+  span.appendChild(link)
+  
+  return article
 }

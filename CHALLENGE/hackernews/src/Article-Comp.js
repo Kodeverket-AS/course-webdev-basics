fd13c154e7f23ada429e6c3e1d1ca7135470705e
stian <stiankarlsen709@gmail.com>
2026-02-25 13:37:41 +0100

    startet å sette på js web komponent

--- CHALLENGE/hackernews/src/Article-Comp.js
+++ CHALLENGE/hackernews/src/Article-Comp.js
@@ -1,9 +1,19 @@
-import "./style.css"
-import {getNewsById} from "./NewsById"
+// import {getNewsById} from "./NewsById"
 
-export function ArticleElement(Object){
-   
+class CreateArticleElement extends HTMLElement{
+    constructor(){
+        super();
+        this.innerHTML = "Komponent"
+    }
 }
+
+customElements.define("article-comp", CreateArticleElement)
 
 
 // bruk = ArticleElement()
+
+// -Web Components- https://www.youtube.com/watch?v=2I7uX8m0Ta0
+
+// -Using custom elements- https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements
+// -Using shadow DOM- https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_shadow_DOM
+// -Using templates and slots- https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_templates_and_slots

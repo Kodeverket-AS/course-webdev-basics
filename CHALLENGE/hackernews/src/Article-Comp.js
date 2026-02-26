80fe3c2fbf280bdf844364681d089692114729e1
stian <stiankarlsen709@gmail.com>
2026-02-26 14:07:12 +0100

    safe save

--- CHALLENGE/hackernews/src/Article-Comp.js
+++ CHALLENGE/hackernews/src/Article-Comp.js
@@ -1,12 +1,70 @@
 import {NewsById} from "./main.js"
 
-
-
 class CreateArticleElement extends HTMLElement{
+
     constructor(){
         super();
-        this.innerHTML = "Komponent"
+        const shadow = this.attachShadow({mode: "open"});
+
+        // Statisk innhold
+        const wrapper = document.createElement("section");
+        wrapper.setAttribute("class", "wrapper");
+
+        const title = document.createElement("h3");
+        title.setAttribute("class", "news-title");
+        title.textContent = "Laster..." // Placeholder
+
+        const para = document.createElement("p");
+        para.setAttribute("class", "para");
+        this.parentElement.textContent = "Content on its way";
+        
+        const style = document.createElement("style");
+        style.textContent = `
+        .wrapper{
+        border:solid 1px var(--news-border);
+        padding: 5px;
+        margin: 10px 0px;
+        border-radius: var(--br);
+        background:var(--fg);
+        }
+
+        .news-title{
+        font-size: 1.2rem;
+        color: var(--text);
+        }
+
+        .para{
+        font-size: 1rem;
+        color: var(--text);
+        }
+        `
+        wrapper.appendChild(title);
+        wrapper.appendChild(para); 
+        this.shadowRoot.appendChild(style);
+        this.shadowRoot.appendChild(wrapper);
+    };
+
+    async Conntent(){
+        try{
+            const id = this.getAttribute("news-id")
+            const data = await NewsById(id);
+            if(!data) {
+                console.log("No data at article comp")
+                return null;
+            };
+
+            // oppdater kun deler som er dynamisk
+            const title = this.shadowRoot.querySelector(".news-title");
+            title.textContent = data.title;
+
+            const para = this.getAttribute("para");
+            para.textContent = data.p;
+
+        }catch (err){
+            console.log("catch error")
+        }
     }
+
 }
 
 customElements.define("article-comp", CreateArticleElement)
--- CHALLENGE/hackernews/src/main.js
+++ CHALLENGE/hackernews/src/main.js
@@ -1,10 +1,20 @@
-
-export async function NewsById(id){
-    const url = "https://hacker-news.firebaseio.com/v0/newstories.json";
+export async function NewsById(){
+    
 try {
-const response = await fetch(url)
+const response = await fetch(`https://hacker-news.firebaseio.com/v0/newstories.json`);
+if(!response.ok){
+    throw new Error("No response");
+}
 
+const data = await response.json();
+console.log(data);
+return 
 } catch (error){
-    console.log("error")
+    console.log("Could not get data")
+    return null;
 }
 }
+
+
+
+

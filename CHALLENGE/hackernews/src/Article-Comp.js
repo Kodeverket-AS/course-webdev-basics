9f571e6b651800482cbe2cc2a5f3daede89af66a
stian <stiankarlsen709@gmail.com>
2026-03-04 12:30:43 +0100

    challenge hacker news, fått design opp, og displayer info fra api i web comp

--- CHALLENGE/hackernews/src/Article-Comp.js
+++ CHALLENGE/hackernews/src/Article-Comp.js
@@ -1,89 +1,113 @@
 
 import { getNewest, getNewsById } from "./api";
 
-//! Hent Data og legg til comp
-async function renderData() {
-  
-}
 
-
-
-// Dette laget skal kun ta seg av å Rendre data (UI-lag). 
-//! Det visuelle laget.
 class CreateArticleElement extends HTMLElement{
     constructor(){
         super();
-        // Oppretter Shadow DOM (isolert DOM-tre)
         this.attachShadow({mode: "open"});
-
-
-        const d = getNewsById(1);
-        console.log(d);
-
-       this.innerHTML = `
-      <style>
-      .article-cont{
-      display:flex;
-      flex-direction:column;
-      }
-
-      .top{}
-
-      .bottom{}
-
-
-      </style>
-
-      <section className="article-cont">
-
-      <div className="top">
-       <p id="id">${}</p>
-       <a href="${}" id="title">${}</a>
-      </div>
-
-      <div className="bottom">
-        <p id="score">${}</p>
-        <p id="by">${}</p>
-        <p id="time">${}</p>
-      </div>
-      </section>
-       ` 
-        const style = document.createElement("style");
-        style.textContent = `
-        .wrapper{
-        border:solid 1px var(--news-border);
-        padding: 5px;
-        margin: 10px 0px;
-        border-radius: var(--br);
-        background:var(--bg);
-        }
-
-        .news-title{
-        font-size: 1.2rem;
-        color: var(--text);
-        }
-
-        .para{
-        font-size: 1rem;
-        color: var(--text);
-        }
-        `
-        // Setter sammen 
-       
     };
-    
 
-};
+    async connectedCallback(){
+      await this.renderData()
+    }
+
+    async renderData(){
+      const ids = await getNewest();
+      const top10 = ids.slice(0, 10);
+      const articles = await Promise.all(top10.map(id => getNewsById(id)))
+      
+      
+        this.shadowRoot.innerHTML = `
+        ${articles.map((article, index) => 
+          `
+          <div class="wrapper">
+          <div class="top">
+            <p class="id">${index + 1}</p>
+            <a href="${article.url ?? "#"}" id="title">${article.title}</a>
+          </div>
+
+          <div class="bottom">
+            <p id="score">Score: ${article.score} |</p>
+            <p id="by">By: ${article.by} |</p>
+            <p id="time">Time: ${article.time} |</p>
+          </div>
+          </div>
+          `
+        ).join("")}
+        
+
+        <style>
+        .wrapper{
+          display: flex;
+          flex-direction: column;
+          border:solid 1px var(--news-border);
+          padding: 5px;
+          margin: 10px 0px;
+          border-radius: var(--br);
+          background:var(--bg);
+          }
+
+        .top{
+           display: flex;
+           flex-direction: row;
+           justify-items: start;
+           align-items: center; 
+           gap: 1rem;
+        }
+
+        .bottom{ 
+          display: flex;
+          flex-direction: row;
+          justify-items: start;
+          align-items: center; 
+          
+         }
+        .id {
+          color:rgb(247, 247, 247);
+          text-decoration: none;
+          font: 500;
+          font-size: larger;
+        }
+
+        #title{
+          color: rgb(247, 247, 247);
+          text-decoration: none;
+          font: 500;
+          font-size: larger;
+        }
+
+
+        #score{
+          color: rgb(136, 136, 136);
+          text-decoration: none;
+          font: 300;
+          font-size: smal;
+        }
+
+        #by{
+          color: rgb(136, 136, 136);
+          text-decoration: none;
+          font: 300;
+          font-size: smal;
+          padding: 0.5rem;
+        }
+
+        #time{
+          color: rgb(136, 136, 136);
+          text-decoration: none;
+          font: 300;
+          font-size: smal;
+          margin: 0.5rem;
+        }
+        </style>
+        ` 
+    }
+    
+  };
+  
 
 customElements.define("create-article", CreateArticleElement);
 
 
-//! RENDERING
 
-
-
-// -Web Components- https://www.youtube.com/watch?v=2I7uX8m0Ta0
-
-// -Using custom elements- https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_custom_elements
-// -Using shadow DOM- https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_shadow_DOM
-// -Using templates and slots- https://developer.mozilla.org/en-US/docs/Web/API/Web_components/Using_templates_and_slots
